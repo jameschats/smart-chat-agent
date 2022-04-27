4212af746a4c458e345ffd8ca73d5872fe50badd
JAMES FREDRIC <jfredric@noura.ai>
2022-04-28 00:31:13 +0300

    css

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
